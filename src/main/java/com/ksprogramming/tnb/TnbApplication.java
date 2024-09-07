3768f2300a5804ab94c47546e232f82fd1c6f89e
SadowskiKrystian <Ksprogramming@outlook.com>
2024-09-07 12:14:01 +0100

    init

---
+++ src/main/java/com/ksprogramming/tnb/TnbApplication.java
@@ -0,0 +1,13 @@
+package com.ksprogramming.tnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TnbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksprogramming/tnb/TnbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksprogramming.tnb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TnbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
